ba52f428ac7b8fc2b093e1ab6183d25354f228c6
DhanSKore99 <dhanashrighotkar99@gmail.com>
2024-06-04 12:32:49 -0400

    getAllCustomers API added in controller,service layers by dhanashri

--- MyCustomerPracticeWithAllRequirements/src/main/java/com/cjc/servicei/CustomerServiceI.java
+++ MyCustomerPracticeWithAllRequirements/src/main/java/com/cjc/servicei/CustomerServiceI.java
@@ -1,9 +1,17 @@
 package com.cjc.servicei;
 
+import java.util.List;
+
 import com.cjc.model.Customer;
 
 public interface CustomerServiceI {
 
 	void saveCustomer(Customer c);
+
+	List<Customer> getCustomers();
+
+	
+	
+
 
 }
